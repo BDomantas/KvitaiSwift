717d91c06ad4b8e7de542f7295bc722b94c5dec8
domantas <domantas.bernatavicius@gmail.com>
2022-06-21 14:07:00 +0300

    Initial Commit

---
+++ kvitai/kvitaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kvitaiApp.swift
+//  kvitai
+//
+//  Created by Domantas Bernatavicius on 2022-06-21.
+//
+
+import SwiftUI
+
+@main
+struct kvitaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
